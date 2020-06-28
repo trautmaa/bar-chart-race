ea4a39d7a664d533f4d4c4764bf59406b64eef7a
Alex Trautman <alexander.trautman@honeywell.com>
2020-06-27 20:42:23 -0400

    Stick color to state

--- barChartRace.py
+++ barChartRace.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 from dataCleaning import *
 from constants import *
 
-rawdf = getDataFrame(PATHS)
-s = rawdf.loc['2020-06-21']
+dataframe = getDataFrame(PATHS)
+s = dataframe.loc['2020-06-21']
 
 # Create single chart
 # fig, ax = plt.subplots(figsize=(4, 2.5), dpi=144)
-colors = plt.cm.Dark2(range(6))
+colors = plt.cm.Dark2(range(50))
 
 
 # Get data for chart
@@ -22,8 +22,9 @@ number_of_cases = s.values
 fig, ax_array = plt.subplots(nrows=1, ncols=3, figsize=(7, 2.5), dpi=144, tight_layout=True)
 dates = ['06-21-2020', '06-22-2020', '06-23-2020']
 for ax, date in zip(ax_array, dates):
-    s = rawdf.loc[date].sort_values()
-    ax.barh(y=s.index, width=s.values, color=colors)
+    ranked_states = dataframe.loc[date]
+    y = dataframe.loc[date].rank(method='first').values
+    ax.barh(y=y, width=ranked_states.values, color=colors, tick_label=ranked_states.index)
     ax.set_title(date, fontsize='smaller')
     beautify_axes(ax)
 
