972f0a475dfbab89b728b4453c606f1923c19ebe
Alex Trautman <alexander.trautman@honeywell.com>
2020-06-28 16:10:15 -0400

    made a movie
    q

--- barChartRace.py
+++ barChartRace.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from dataCleaning import *
 from constants import *
 from prepareData import *
+from matplotlib.animation import FuncAnimation
+from IPython.display import HTML
 
 dataframe = getDataFrame(PATHS)
 s = dataframe.loc['2020-06-21']
@@ -18,17 +20,42 @@ df_expanded, df_rank_expanded = prepare_data(df2)
 
 colors = plt.cm.Dark2(range(50))
 
-# Plot each step of the transition
-fig, ax_array = plt.subplots(nrows=1, ncols=6, figsize=(12,2), dpi=144, tight_layout=True)
+#################################################################
+# Plot each step of the transition next to each other
+# fig, ax_array = plt.subplots(nrows=1, ncols=6, figsize=(12,2), dpi=144, tight_layout=True)
 labels = df_expanded.columns
-for i, ax in enumerate(ax_array.flatten()):
+# for i, ax in enumerate(ax_array.flatten()):
+#     y = df_rank_expanded.iloc[i]
+#     width = df_expanded.iloc[i]
+#     ax.barh(y=y, width=width, color=colors, tick_label=labels)
+#     beautify_axes(ax)
+
+# ax_array[0].set_title('one')
+# ax_array[-1].set_title('two')
+
+# Render chart
+# plt.show() 
+
+def init():
+    ax.clear()
+    beautify_axes(ax)
+    ax.set_ylim(.2, 6.8)
+
+def update(i):
+    for bar in ax.containers:
+        bar.remove()
     y = df_rank_expanded.iloc[i]
     width = df_expanded.iloc[i]
     ax.barh(y=y, width=width, color=colors, tick_label=labels)
-    beautify_axes(ax)
+    date_str = df_expanded.index[i].strftime('%B %-d, %Y')
+    ax.set_title(f'COVID Deaths By State - {date_str}', fontsize='smaller')
 
-ax_array[0].set_title('one')
-ax_array[-1].set_title('two')
+fig = plt.Figure(figsize=(4, 2.5), dpi=144)
+ax = fig.add_subplot()
+anim = FuncAnimation(fig=fig, func=update, init_func=init, frames=len(df_expanded), interval=100, repeat=False)
+print(anim)
+HTML(anim.to_html5_video())
 
-# Render chart
-plt.show() 
+anim.save('covid19.mp4')
+
+
